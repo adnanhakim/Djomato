f4b246205fe3fc29703fa3c0b3265731b29bf67c
Adnan Hakim <adnanhakim007@gmail.com>
2019-03-19 22:24:17 +0530

    Working Zomato Url (2)

--- Djomato/DJEats/views.py
+++ Djomato/DJEats/views.py
@@ -9,9 +9,14 @@ longitude = 72.860687
 def home(request):
     url = 'https://developers.zomato.com/api/v2.1/geocode?lat={}&lon={}'
     header = {
-        'user-key': 'cc74cee4a73688e98909b2e1d59cbfd6'
+        'user-key': api_key
     }
-    response = requests.get(url.format(latitude, longitude), headers=header)
-    new_url = url.format(latitude, longitude)
-    print(response.text)
+    response = requests.get(url.format(latitude, longitude), headers=header).json()
+
+    restaurant: {
+        'name': response['nearby_restaurants'][0]['restaurant']['name'],
+    }
+
+    print(restaurant.get('name'))
+
     return render(request, 'DJEats/home.html')
