7c4714fe770cd6c48b24b55d0ffedec3aa8b30b6
Adnan Hakim <adnanhakim007@gmail.com>
2019-03-19 20:14:18 +0530

    Templates added

--- Djomato/Djomato/settings.py
+++ Djomato/Djomato/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'DJEats.apps.DjeatsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
