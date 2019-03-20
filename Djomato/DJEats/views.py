8e6a459a018964b3c1f557eab8f6438b5b5fabab
Adnan Hakim <adnanhakim007@gmail.com>
2019-03-20 21:52:39 +0530

    Footer

--- Djomato/DJEats/views.py
+++ Djomato/DJEats/views.py
@@ -24,7 +24,8 @@ def home(request):
             'id': restaurant_obj['id'],
             'name': restaurant_obj['name'],
             'locality': restaurant_obj['location']['locality'],
-            'image': restaurant_obj['featured_image']
+            'image': restaurant_obj['featured_image'],
+            'cost': restaurant_obj['currency'] + str(restaurant_obj['average_cost_for_two'])
         }
         restaurants.append(restaurant)
 
