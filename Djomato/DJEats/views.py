90d58d4da133ceb4fb63b4b298519d4f1e571161
Adnan Hakim <adnanhakim007@gmail.com>
2019-03-21 15:16:17 +0530

    Debug

--- Djomato/DJEats/views.py
+++ Djomato/DJEats/views.py
@@ -1,4 +1,5 @@
 import requests
+from django.http import HttpResponse
 from django.shortcuts import render
 
 api_key = 'cc74cee4a73688e98909b2e1d59cbfd6'
@@ -7,34 +8,7 @@ longitude = 72.860687
 
 
 def home(request):
-    url = 'https://developers.zomato.com/api/v2.1/geocode?lat={}&lon={}'
-    header = {
-        'user-key': api_key
-    }
-    response = requests.get(url.format(latitude, longitude), headers=header).json()
-
-    location = response['location']['title']
-    restaurant_array = response['nearby_restaurants']
-    length = len(restaurant_array)
-
-    restaurants = []
-    for i in range(0, length):
-        restaurant_obj = restaurant_array[i]['restaurant']
-        restaurant = {
-            'id': restaurant_obj['id'],
-            'name': restaurant_obj['name'],
-            'locality': restaurant_obj['location']['locality'],
-            'image': restaurant_obj['featured_image'],
-            'cost': restaurant_obj['currency'] + str(restaurant_obj['average_cost_for_two'])
-        }
-        restaurants.append(restaurant)
-
-    context = {
-        'location': location,
-        'restaurants': restaurants
-    }
-
-    return render(request, 'DJEats/home.html', context)
+    return render(request, 'DJEats/home.html')
 
 
 def details(request):
