7fb2472ab7ff5e1b1b8db524f9f8069ca2f2910c
Adnan Hakim <adnanhakim007@gmail.com>
2019-03-21 15:23:45 +0530

    debyg 2

--- Djomato/DJEats/views.py
+++ Djomato/DJEats/views.py
@@ -8,7 +8,34 @@ longitude = 72.860687
 
 
 def home(request):
-    return render(request, 'DJEats/home.html')
+    url = 'https://developers.zomato.com/api/v2.1/geocode?lat={}&lon={}'
+    header = {
+        'user-key': api_key
+    }
+    response = requests.get(url.format(latitude, longitude), headers=header).json()
+
+    location = response['location']['title']
+    restaurant_array = response['nearby_restaurants']
+    length = len(restaurant_array)
+
+    restaurants = []
+    for i in range(0, length):
+        restaurant_obj = restaurant_array[i]['restaurant']
+        restaurant = {
+            'id': restaurant_obj['id'],
+            'name': restaurant_obj['name'],
+            'locality': restaurant_obj['location']['locality'],
+            'image': restaurant_obj['featured_image'],
+            'cost': restaurant_obj['currency'] + str(restaurant_obj['average_cost_for_two'])
+        }
+        restaurants.append(restaurant)
+
+    context = {
+        'location': location,
+        'restaurants': restaurants
+    }
+
+    return render(request, 'DJEats/home.html', context)
 
 
 def details(request):
